3f21749b26d04e7e35c6b82620f1cb884f149a3a
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-27 19:50:25 -0300

    resolucao do exercicio 01

--- ex01.js
+++ ex01.js
@@ -1,1 +1,13 @@
-console.log("Primeiro commit do repositório!");
+const prompt = require("prompt-sync")();
+console.clear();
+
+let num1 = +prompt("Digite o primeiro número: ");
+let num2 = +prompt("Digite o segundo número: ");
+
+if (num1 > num2) {
+  console.log("O primeiro número é o maior.");
+} else if (num1 < num2) {
+  console.log("O segundo número é o maior.");
+} else {
+  console.log("Os números digitados são iguais.");
+}
